1d3071d27f875bd4851a35534a160d55bd17557a
KalyanChakravarthyV <KalyanChakravarthyV@gmail.com>
2021-11-27 23:05:02 +0530

    Update to add-epel-repo.sh $ needs to be escaped

--- oci/add-epel-repo.sh
+++ oci/add-epel-repo.sh
@@ -2,8 +2,8 @@
 
 cat > /etc/yum.repos.d/epel-yum-ol7.repo << EOL
 [ol7_epel]
-name=Oracle Linux $releasever EPEL ($basearch)
-baseurl=http://yum.oracle.com/repo/OracleLinux/OL7/developer_EPEL/$basearch/
+name=Oracle Linux \$releasever EPEL (\$basearch)
+baseurl=http://yum.oracle.com/repo/OracleLinux/OL7/developer_EPEL/\$basearch/
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-oracle
 gpgcheck=1
 enabled=1
